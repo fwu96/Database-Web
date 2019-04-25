75a30d31490d661b840dade46a0a09a79f7fd171
Feifan Wu <fwu62@wisc.edu>
2019-04-25 13:04:59 -0500

    Update sqlitedb.py

--- auctionbase/web.py/sqlitedb.py
+++ auctionbase/web.py/sqlitedb.py
@@ -33,9 +33,9 @@ def getTime():
     # TODO: update the query string to match
     # the correct column and table name in your database
     query_string = 'select Time from CurrentTime'
-    results = query(query_string)
-    # alternatively: return results[0]['currenttime']
-    return results[0].Time # TODO: update this as well to match the
+    dist_id = query(query_string)
+    # alternatively: return dist_id[0]['currenttime']
+    return dist_id[0].Time # TODO: update this as well to match the
                                   # column name
 
 # returns a single item specified by the Item's ID in the database
@@ -102,9 +102,12 @@ def browseAuctions(itemid, category, itemdes, min_price,max_price, status):
     itemdes.encode("utf-8")
     min_price.encode("utf-8")
     max_price.encode("utf-8")
-    results = [] 
-    result_auction = []
-    result_cat = []
+    dist_id = [] 
+    info_result = []
+    cat_result = []
+    status_result = []
+    bid_result = []
+    winner_result = []
     query_string = 'select distinct Items.itemid from Items, Categories  where Items.ItemID = Categories.ItemID'
     if itemid != '' :
         query_string = query_string + ' and Items.ItemID = ' + itemid 
@@ -121,14 +124,47 @@ def browseAuctions(itemid, category, itemdes, min_price,max_price, status):
     elif(status == 'close'):
         query_string = query_string + ' and (Items.Ends <= \'' + getTime().encode("utf-8") + '\' or ((Items.Currently >= Items.Buy_Price and Items.Buy_Price is not null) or Items.Buy_Price is null) or Items.Started > \'' +getTime().encode("utf-8") +'\')'
     try:
-        results = query(query_string)
-        for i in range (0, len(results) - 1):
-            q_str = "select * from Items where itemid = " + results[i].itemid.encode("utf-8")
-            result2 = query(q_str)
-            result_auction.append(result2)
-            q2_str = "select * from Categories where itemid = " + results[i].itemid.encode("utf-8")
-            result3 = query(q2_str)
-            result_cat.append(result3)
+        dist_id = query(query_string)
+        #print "dist_id[0] item id is: "
+        #print dist_id[0].ItemID
+        #print "after try excute query, result = "
+        #print dist_id
+        #print "dist_id length = "
+        #print len(dist_id)
+        for i in range (len(dist_id)):
+            #print "in for loop"
+            q_info = "select *, 'link' as Link from Items where itemid = " + str(dist_id[i].ItemID).encode("utf-8")
+            #print q_info
+            info_result = query(q_info)
+            #print info_result
+            #result_auction.append(info_result)
+            q_cat = 'select group_concat(category, \' ,\' ) as Category from Categories where itemid = ' + str(dist_id[i].ItemID).encode("utf-8") + ' group by itemID'
+            #print q_cat
+            cat_result = query(q_cat)
+            #print cat_result
+            #result_cat.append(cat_result)
+            q_status = ('select (case when (Items.Ends > \'' + getTime().encode("utf-8") + '\' and '
+                        '((Items.Currently < Items.Buy_Price and Items.Buy_Price is not null) or Items.Buy_Price is null) '
+                        'and Items.Started <= \'' + getTime().encode("utf-8") + '\') then \'Open\' '
+                        'when ((Items.Ends <= \'' + getTime().encode("utf-8") + '\' or '
+                        '((Items.Currently >= Items.Buy_Price and Items.Buy_Price is not null) or '
+                        'Items.Buy_Price is null) or Items.Started > \'' + getTime().encode("utf-8") + '\')) then \'Close\' '
+                        'else \'All\' end) '
+                        'as Status from Items where itemId = ' + str(dist_id[i].ItemID).encode("utf-8"))
+            status_result = query(q_status)
+            #print status_result
+            q_bid = ('select \'Name: \' || userID || \'; \' || \'Time: \' || Time || \'; \' || \'Price: \' || Amount as Bid '
+                     'from Bids where itemID = ' + str(dist_id[i].ItemID).encode("utf=8") + ' group by userID order by Amount ASC')
+            bid_result = query(q_bid)
+            print bid_result
+            if len(bid_result) != 0:
+                q_winner = ('select \'Name: \' || userID || \'; \' || \'Price: \' || Amount as Winner '
+                            'from Bids where itemID = ' + str(dist_id[i].ItemID).encode("utf-8") + ' and Amount = '
+                            '(select max(Amount) from Bids where itemID = ' + str(dist_id[i].ItemID).encode("utf-8") + ')')
+                winner_result = query(q_winner)
+                print winner_result
     except Exception as e:
+        print "in exception"
         print str(e)
-    return results, result_auction, result_cat 
+    return info_result, cat_result, status_result, bid_result, winner_result
+
